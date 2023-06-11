a3e3546cb5afd2f7a94458a1f9c88fcfb5159a42
Luks245 <florekl03c@lo5.bielsko.pl>
2023-06-11 22:47:20 +0200

    fixing mobile

--- src/components/weather-box.js
+++ src/components/weather-box.js
@@ -1,90 +1,132 @@
-import pick from "../functions/pickSuggestion"
-import search from "../functions/searchFunc"
-import { useRef, useState, useEffect } from "react"
-import WeatherContent from "./weather_content"
-import Forecast from "./weather-forecast"
+import pick from "../functions/pickSuggestion";
+import search from "../functions/searchFunc";
+import { useRef, useState, useEffect } from "react";
+import WeatherContent from "./weather_content";
+import Forecast from "./weather-forecast";
 
+export default function WeatherBox({
+  searchSuggestions,
+  setCurrentWeather,
+  setWeatherForecast,
+  setCity,
+  city,
+  currentWeather,
+  setPicked,
+  list,
+  weatherForecast,
+  setHomeScreen,
+}) {
+  const citySearchRef = useRef(null);
+  const dropdownRef = useRef(null);
+  const [loaded, setLoaded] = useState(true);
 
-export default function WeatherBox({searchSuggestions, setCurrentWeather, setWeatherForecast, setCity, city, currentWeather, setPicked, list, weatherForecast, setHomeScreen}){
-    const citySearchRef = useRef(null)
-    const dropdownRef = useRef(null)
-    const [loaded, setLoaded] = useState(true)
-    
-    useEffect(() => {
-        if(!loaded)
-        search({setCurrentWeather, setWeatherForecast, city, setLoaded,setHomeScreen})
-    },[loaded])
+  useEffect(() => {
+    if (!loaded)
+      search({
+        setCurrentWeather,
+        setWeatherForecast,
+        city,
+        setLoaded,
+        setHomeScreen,
+      });
+  }, [loaded]);
 
-    function searchPress(){
-        if(list.length <= 0) return
-        setLoaded(false)
-        dropdownRef.current.style.zIndex = '-1'
-        document.documentElement.style.setProperty('--box-height','650px')
-        document.documentElement.style.setProperty('--weather-content-opacity',1)
-    }
+  function searchPress() {
+    if (list.length <= 0) return;
+    setLoaded(false);
+    dropdownRef.current.style.zIndex = "-1";
+    document.documentElement.style.setProperty("--box-height", "650px");
+    document.documentElement.style.setProperty("--weather-content-opacity", 1);
+  }
 
-    return(
-        <main>
-            <section id="weather-box-section">
-                <div id="input-box"> 
-                    <input
-                        type="text"
-                        placeholder="Enter a city"
-                        onChange={(e) => setCity(e.target.value)}
-                        onFocus={() => {
-                            setHomeScreen(true)
-                            dropdownRef.current.style.zIndex = '1'
-                            document.documentElement.style.setProperty('--box-height','40px')
-                            document.documentElement.style.setProperty('--weather-content-opacity',0)
-                        }}
-                        onKeyDown={(e) => {
-                            if(e.key === "Enter"){
-                                searchPress()
-                                e.target.blur()
-                            }
-                        }}
-                        ref={citySearchRef}
-                    />
-                    <button onClick={searchPress}><i className="fa fa-magnifying-glass"/></button>
-                </div>
-               
-                {currentWeather.main !== undefined ?
-                    <WeatherContent 
-                        currentWeather={currentWeather}
-                        city={city}
-                    />
-                    :
-                    loaded ?
-                    <div className="no-results">
-                        <img src="https://cdni.iconscout.com/illustration/premium/thumb/search-result-not-found-2130355-1800920.png" width="80%"></img>
-                        <h1 style={{color:'white'}}>Oops! No matches found.</h1>
-                    </div>
-                    :
-                    <div></div>
-                }
+  return (
+    <main>
+      <section id="weather-box-section">
+        <div id="input-box">
+          <input
+            type="text"
+            placeholder="Enter a city"
+            onChange={(e) => setCity(e.target.value)}
+            onFocus={() => {
+              setHomeScreen(true);
+              dropdownRef.current.style.zIndex = "1";
+              document.documentElement.style.setProperty(
+                "--box-height",
+                "40px"
+              );
+              document.documentElement.style.setProperty(
+                "--weather-content-opacity",
+                0
+              );
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                searchPress();
+                e.target.blur();
+              }
+            }}
+            ref={citySearchRef}
+          />
+          <button onClick={searchPress}>
+            <i className="fa fa-magnifying-glass" />
+          </button>
+        </div>
 
-                {currentWeather.main !== undefined && 
-                    <Forecast weatherForecast={weatherForecast} currentWeather={currentWeather}/>
-                }
-                {(!loaded && currentWeather.main === undefined)&& 
-                    <div className="lds-facebook"><div></div><div></div><div></div></div>
-                }
+        {currentWeather.main !== undefined ? (
+          <WeatherContent currentWeather={currentWeather} city={city} />
+        ) : loaded ? (
+          <div className="no-results">
+            <img
+              src="https://cdni.iconscout.com/illustration/premium/thumb/search-result-not-found-2130355-1800920.png"
+              width="80%"
+            ></img>
+            <h1 style={{ color: "white" }}>Oops! No matches found.</h1>
+          </div>
+        ) : (
+          <div></div>
+        )}
 
-            </section>
-            
-            <div id="dropdown-suggestions" ref={dropdownRef}>
-                {searchSuggestions.map(item => {
-                    return(
-                        <div key={Math.random()} id="suggestion" onClick={() => pick({name:item.name,country:item.country, setCity, citySearchRef, setPicked})}>
-                            <div>
-                                <span>{item.name}</span>
-                                <span>{item.state}</span>
-                            </div>
-                            <span>{item.country}</span>
-                        </div>
-                    )
-                })}
-            </div> 
-        </main>
-    )
+        {currentWeather.main !== undefined && (
+          <Forecast
+            weatherForecast={weatherForecast}
+            currentWeather={currentWeather}
+          />
+        )}
+        {!loaded && currentWeather.main === undefined && (
+          <div className="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        )}
+        <div id="margin"></div>
+      </section>
+
+      <div id="dropdown-suggestions" ref={dropdownRef}>
+        {searchSuggestions.map((item) => {
+          return (
+            <div
+              key={Math.random()}
+              id="suggestion"
+              onClick={() =>
+                pick({
+                  name: item.name,
+                  country: item.country,
+                  setCity,
+                  citySearchRef,
+                  setPicked,
+                })
+              }
+            >
+              <div>
+                <span>{item.name}</span>
+                <span>{item.state}</span>
+              </div>
+              <span>{item.country}</span>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
--- src/functions/calculateGraph.js
+++ src/functions/calculateGraph.js
@@ -1,32 +1,33 @@
-export default function calculateGraph({max,min}){
-    const averageTemps = []
+export default function calculateGraph({ max, min }) {
+  const averageTemps = [];
 
-    max.forEach((item,index) => {
-        averageTemps.push((item+min[index])/2)
-    })
-    const maxValue = Math.max(...averageTemps)
-    const minValue = Math.min(...averageTemps)
+  max.forEach((item, index) => {
+    averageTemps.push((item + min[index]) / 2);
+  });
+  const maxValue = Math.max(...averageTemps);
+  const minValue = Math.min(...averageTemps);
 
-    const range = maxValue - minValue
-    const coefficient = 25/range
+  const range = maxValue - minValue;
+  const coefficient = 25 / range;
+  console.log(coefficient);
 
-    const differences = []
-    averageTemps.forEach((item,index) => {
-        if(index >= 4){
-            differences.push({deg:0, mean:0})
-            return
-        }
-        const deg = coefficient * Math.atan((item - averageTemps[index+1]),107)
-        differences.push(
-            {
-                deg: deg,
-                mean: ((item + averageTemps[index+1])/2 - minValue) * coefficient + 22.5,
-            }
-        )
-    })
-    averageTemps.forEach((item,index) => averageTemps[index] = (item-minValue)*coefficient + 20.5)
+  const differences = [];
+  averageTemps.forEach((item, index) => {
+    if (index >= 4) {
+      differences.push({ deg: 0, mean: 0 });
+      return;
+    }
+    const deg = coefficient * Math.atan(item - averageTemps[index + 1], 107);
+    differences.push({
+      deg: deg,
+      mean:
+        ((item + averageTemps[index + 1]) / 2 - minValue) * coefficient + 22.5,
+    });
+  });
+  averageTemps.forEach(
+    (item, index) =>
+      (averageTemps[index] = (item - minValue) * coefficient + 20.5)
+  );
 
-    
-
-    return {diff: differences, average: averageTemps}
+  return { diff: differences, average: averageTemps };
 }
