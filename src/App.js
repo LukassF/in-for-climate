f34aee48779cbd73dcfa6ad4193641419f5bcdd0
Luks245 <florekl03c@lo5.bielsko.pl>
2023-06-28 13:30:06 +0200

    modified styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import match from "./functions/matchCities";
-import './styles/main.css'
+import "./styles/main.css";
 import WeatherBox from "./components/weather-box";
 import Navbar from "./components/navbar";
 import Logo from "./components/logo";
@@ -8,31 +8,34 @@ import { CSSTransition } from "react-transition-group";
 
 export default function App() {
   const [city, setCity] = useState("");
-  const [currentWeather, setCurrentWeather] = useState({})
-  const [weatherForecast, setWeatherForecast] = useState([])
-  const [list, setList] = useState([])
-  const [picked, setPicked] = useState(false)
-  const [homeScreen, setHomeScreen] = useState(true)
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [weatherForecast, setWeatherForecast] = useState([]);
+  const [list, setList] = useState([]);
+  const [picked, setPicked] = useState(false);
+  const [homeScreen, setHomeScreen] = useState(true);
 
   useEffect(() => {
-    if(city.length > 0){
-        match({city,setList})
-    }else{setList([])}
-    setCurrentWeather({})
-  },[city])
+    if (city.length > 0) {
+      match({ city, setList });
+    } else {
+      setList([]);
+    }
+    setCurrentWeather({});
+  }, [city]);
 
   useEffect(() => {
-    if(list.length>0 && !picked) document.documentElement.style.setProperty('--dropdown-opacity', 1)
-    else document.documentElement.style.setProperty('--dropdown-opacity', 0)
-    setPicked(false)
-  },[list])
+    if (list.length > 0 && !picked)
+      document.documentElement.style.setProperty("--dropdown-opacity", 1);
+    else document.documentElement.style.setProperty("--dropdown-opacity", 0);
+    setPicked(false);
+  }, [list]);
 
   return (
     <>
       <Navbar />
-      <WeatherBox 
+      <WeatherBox
         searchSuggestions={list}
-        setCurrentWeather={setCurrentWeather}        
+        setCurrentWeather={setCurrentWeather}
         setWeatherForecast={setWeatherForecast}
         setCity={setCity}
         currentWeather={currentWeather}
@@ -41,11 +44,16 @@ export default function App() {
         list={list}
         weatherForecast={weatherForecast}
         setHomeScreen={setHomeScreen}
-        />
+      />
 
-        <CSSTransition in={homeScreen} timeout={300} unmountOnExit classNames='home-screen-transition'>
-          <Logo />
-        </CSSTransition>
+      <CSSTransition
+        in={homeScreen}
+        timeout={300}
+        unmountOnExit
+        classNames="home-screen-transition"
+      >
+        <Logo />
+      </CSSTransition>
     </>
   );
 }
